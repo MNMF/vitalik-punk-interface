af22eb0f3792708af3c50149815d2c7118ec657b
MNMF <mmonsalve15@alumnos.utalca.cl>
2021-11-24 15:52:27 -0300

    conexion con metamask funcionando

--- src/config/web3/index.js
+++ src/config/web3/index.js
@@ -1,17 +1,14 @@
-import Web3 from 'web3'
-import { InjectedConnector } from '@web3-react/injected-connector'
+import Web3 from "web3";
+import { InjectedConnector } from "@web3-react/injected-connector";
 
 const connector = new InjectedConnector({
-     supportedChainIds: [
-         1, 3, 4, 5, 42
-        ]
-     })
+  supportedChainIds: [
+    4, // Rinkeby
+  ],
+});
 
+const getLibrary = (provider) => {
+  return new Web3(provider);
+};
 
-
-const getLibrery = (provider) => {
-    return new Web3(provider);
-
-}
-
-export { connector, getLibrery };
+export { connector, getLibrary };
--- src/index.js
+++ src/index.js
@@ -3,17 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-import { Web3ReactProvider } from '@web3-react/core';
-import {getLibrery} from './config/web3'
-
+import { Web3ReactProvider } from "@web3-react/core";
+import { getLibrary } from "./config/web3";
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
       <ChakraProvider>
-        <web3ReactProvider getLibrery={getLibrery}>
-        <App />
-        </web3ReactProvider>
+        <Web3ReactProvider getLibrary={getLibrary}>
+          <App />
+        </Web3ReactProvider>
       </ChakraProvider>
     </HashRouter>
   </React.StrictMode>,
